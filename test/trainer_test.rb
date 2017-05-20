9bba053255d44a963ffdb1aaa2e40a3c3c2a19fc
Oliver <oliver.hohn@kcl.ac.uk>
2017-05-20 18:50:31 +0100

    Added a test for successive catching of the same Kudomon

--- test/trainer_test.rb
+++ test/trainer_test.rb
@@ -10,6 +10,7 @@ class TrainerTest < Test::Unit::TestCase
   def setup
     @game = Game.new
     @trainer = Trainer.new("Timmy", 5, 10, @game)
+    @trainer2 = Trainer.new("Bobby", 5, 9, @game)
     @kudomon_1 = Kudomon.new("Kudomon 1", 25, 55, KudomonTypes::WATER, 15)
     @game.add_kudomon(@kudomon_1)
     @game.add_trainer(@trainer)
@@ -32,13 +33,22 @@ class TrainerTest < Test::Unit::TestCase
   end
 
   # Tests catching Kudomons that are being caught by others
-  def test_catching_kudomon
+  def test_catching_caught_kudomon
     @kudomon_2.life_cyc=KudomonLifeCycle::BEING_CAUGHT
     assert_false (@trainer.catch_kudomon(@kudomon_2))
     @kudomon_2.life_cyc= KudomonLifeCycle::CAUGHT
     assert_false (@trainer.catch_kudomon(@kudomon_2))
     @kudomon_2.life_cyc= KudomonLifeCycle::FREE
     assert_true(@trainer.catch_kudomon(@kudomon_2))
+  end
+
+  # Tests two trainers attempting to catch a Kudomon.
+  # First trainer to attempt to catch should be able to, other trainer should not be able to.
+  def test_successive_catching
+    assert_true(@trainer.catch_kudomon(@kudomon_2))
+    assert_false(@trainer2.catch_kudomon(@kudomon_2))
+    assert_true(@trainer.catched_kudomons.include? @kudomon_2)
+    assert_false(@trainer2.catched_kudomons.include? @kudomon_2)
   end
 
   # Called after every test method runs. Can be used to tear
